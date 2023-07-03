893da4f85bfaffc0147615b6fb64e9c6969f3518
dammy995 <damianemerah1@gmail.com>
2023-07-03 15:56:08 +0100

    done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Generates Pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    pascal_triangle: 
+        Solves and generate pascal triangle
+    n:
+        number of triangle to generate
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+
+pascal_triangle(2)
