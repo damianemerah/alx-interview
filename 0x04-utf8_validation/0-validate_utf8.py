2edb0f169ce1ae064435e3c4e74466e7812eade8
dammy995 <damianemerah1@gmail.com>
2023-07-31 03:02:35 +0100

    done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -38,11 +38,6 @@
 #     return True
 
 
-#!/usr/bin/python3
-"""UTF-8 validation module.
-"""
-
-
 def validUTF8(data):
     """Checks if a list of integers are valid UTF-8 codepoints.
     See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
