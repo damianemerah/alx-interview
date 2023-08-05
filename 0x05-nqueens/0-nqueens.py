e014aa91499f589329ff0ed0adce02e5a7de0264
dammy995 <damianemerah1@gmail.com>
2023-08-05 13:25:41 +0100

    nqueen problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,82 +1,73 @@
 #!/usr/bin/python3
-"""
-N Queens Problem Solver
-
-Usage: nqueens N
-
-This program solves the N Queens problem by placing N non-attacking queens on an N×N chessboard.
-
-Arguments:
-    N (int): The size of the chessboard and the number of queens.
-
-Example:
-    To solve the 4 Queens problem:
-    $ ./nqueens.py 4
-
-    To solve the 6 Queens problem:
-    $ ./nqueens.py 6
-"""
-
+""" N QUEENS ALGORITHM WITH BACKTRACKING (RECURSION INSIDE LOOP) """
 import sys
 
 
-def is_safe(board, row, col):
-    '''Check if it's safe to place a queen at board[x][y]'''
-    # Check if there is a queen in the same column
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
+class NQueen:
+    """ Class for solving N Queen Problem """
 
-    # Check if there is a queen in the upper-left diagonal
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+    def __init__(self, n):
+        """ Global Variables """
+        self.n = n
+        self.x = [0 for i in range(n + 1)]
+        self.res = []
 
-    # Check if there is a queen in the upper-right diagonal
-    for i, j in zip(range(row, -1, -1), range(col, len(board))):
-        if board[i][j] == 1:
-            return False
+    def place(self, k, i):
+        """ Checks if k Queen can be placed in i column (True)
+        or if the are attacking queens in row or diagonal (False)
+        """
 
-    return True
+        # j checks from 1 to k - 1 (Up to previous queen)
+        for j in range(1, k):
+            # There is already a queen in column
+            # or a queen in same diagonal
+            if self.x[j] == i or \
+               abs(self.x[j] - i) == abs(j - k):
+                return 0
+        return 1
+
+    def nQueen(self, k):
+        """ Tries to place every queen in the board
+        Args:
+        k: starting queen from which to evaluate (should be 1)
+        """
+        # i goes from column 1 to column n (1st column is 1st index)
+        for i in range(1, self.n + 1):
+            if self.place(k, i):
+                # Queen can be placed in i column
+                self.x[k] = i
+                if k == self.n:
+                    # Placed all 4 Queens (A solution was found)
+                    solution = []
+                    for i in range(1, self.n + 1):
+                        solution.append([i - 1, self.x[i] - 1])
+                    self.res.append(solution)
+                else:
+                    # Need to place more Queens
+                    self.nQueen(k + 1)
+        return self.res
 
 
-def nqueens(board, row):
-    '''Use backtracking to find all solutions'''
-    if row == len(board):
-        solution = []
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if board[i][j] == 1:
-                    solution.append([i, j])
-        print(solution)
-        return
+# Main
 
-    for col in range(len(board)):
-        if is_safe(board, row, col):
-            board[row][col] = 1
-            nqueens(board, row + 1)
-            board[row][col] = 0
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
+N = sys.argv[1]
 
-def solve_nqueens(N):
-    '''Solve the N Queens problem'''
-    if not N.isdigit():
-        print("N must be a number")
-        sys.exit(1)
-
+try:
     N = int(N)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    nqueens(board, 0)
+queen = NQueen(N)
+res = queen.nQueen(1)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    solve_nqueens(sys.argv[1])
+for i in res:
+    print(i)
