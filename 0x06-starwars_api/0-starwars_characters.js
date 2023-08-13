1b3a178ff5cbf44b5cd050a153d5c0e67a70d3c6
root <damianemerah1@gmail.com>
2023-08-13 11:09:39 -0700

    Star wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require("request");
+
+function get_films() {
+  const url = "https://swapi-api.alx-tools.com/api/films";
+  if (process.argv.length > 2) {
+    request(`${url}/${process.argv[2]}`, (err, _, body) => {
+      if (err) console.log(err);
+      const { characters } = JSON.parse(body);
+      const characterNames = characters.map(url =>
+        new Promise((resolve, reject) => {
+          request(url, (promiseErr, _, characterReqBody) => {
+            if (promiseErr) reject(promiseErr);
+            resolve(JSON.parse(characterReqBody).name);
+	  })
+	})
+      );
+      Promise.all(characterNames)
+        .then((name) => console.log(name.join('\n')))
+        .catch(err => console.log(err));
+    })
+  }
+}
+
+get_films();
