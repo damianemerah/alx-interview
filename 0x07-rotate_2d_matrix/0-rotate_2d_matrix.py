8c4b48451e8791e853c714af981d5d82d6644e86
dammy995 <damianemerah1@gmail.com>
2023-08-24 00:48:29 +0100

    Matrix rotate 2d

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -11,10 +11,18 @@ def rotate_2d_matrix(matrix):
         for j in range(i, n):
             # matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
             temp = matrix[i][j]
-            print(temp, i, j)
             matrix[i][j] = matrix[j][i]
             matrix[j][i] = temp
 
     # Reverse each row
     for i in range(n):
         matrix[i] = matrix[i][::-1]
+
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13,
+                                                  14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]
+
+    rotate_2d_matrix(matrix)
+    for row in matrix:
+        print(row)
