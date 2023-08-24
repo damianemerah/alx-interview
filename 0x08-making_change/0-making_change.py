f10c7096afb8cde7fffd32b133310c1a70ff0f81
dammy995 <damianemerah1@gmail.com>
2023-08-24 01:19:20 +0100

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,15 +9,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
-    print(coins)
-    count = 0
-    for coin in coins:
-        if coin <= total:
-            count += total // coin
-            total = total % coin
-        if total == 0:
-            break
-    if total != 0:
-        return -1
-    return count
+    # Initialize a list to store the minimum number of coins needed for each amount
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0
+
+    # Compute the minimum number of coins for each amount from 1 to total
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if amount >= coin:
+                min_coins[amount] = min(
+                    min_coins[amount], min_coins[amount - coin] + 1)
+
+    # If the final value of min_coins[total] is still infinity, it means the total cannot be met
+    return min_coins[total] if min_coins[total] != float('inf') else -1
