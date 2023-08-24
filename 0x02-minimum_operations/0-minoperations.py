bb3b91ab0cabb6e9227914073087ec036d6ab59f
dammy995 <damianemerah1@gmail.com>
2023-08-24 01:38:56 +0100

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -20,18 +20,55 @@ Number of operations: 6
 """
 
 
+# def minOperations(n):
+#     """
+#     Returns the fewest number of operations needed to result in exactly
+#     n H characters in the file.
+#     """
+#     def minOperations(n):
+#         """Calculate fewest no. of operations needed to result in n H characters"""
+#         t = 0
+#         m = 2
+#         while n > 1:
+#             while not n % m:
+#                 t += m
+#                 n /= m
+#             m += 1
+#         return t
+
+
+#!/usr/bin/python3
+'''The minimum operations coding challenge.
+'''
+
+
 def minOperations(n):
-    """
-    Returns the fewest number of operations needed to result in exactly
-    n H characters in the file.
-    """
-    def minOperations(n):
-        """Calculate fewest no. of operations needed to result in n H characters"""
-        t = 0
-        m = 2
-        while n > 1:
-            while not n % m:
-                t += m
-                n /= m
-            m += 1
-        return t
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
