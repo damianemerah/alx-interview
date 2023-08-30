53e0614500d7f9633dcbc34c4b977cf2ef2c4fd1
dammy995 <damianemerah1@gmail.com>
2023-08-30 22:19:32 +0100

    pycodesttyle

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,9 +15,11 @@ def island_perimeter(grid):
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                perimeter += 4  # Start with the maximum perimeter for each land cell
+                # Start with the maximumperimeter for each land cell
+                perimeter += 4
 
-                # Check adjacent cells and subtract 1 for each adjacent land cell
+                # Check adjacent cells and subtract 1 for
+                # each adjacent land cell
                 if r > 0 and grid[r - 1][c] == 1:
                     perimeter -= 1
                 if r < rows - 1 and grid[r + 1][c] == 1:
